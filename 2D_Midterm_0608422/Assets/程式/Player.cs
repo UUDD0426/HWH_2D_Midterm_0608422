b91d82c4b0feab5ccbffc2131b2cbe4c983bbefa
UUDD0426 <71476539+UUDD0426@users.noreply.github.com>
2021-04-23 13:09:36 +0800

    0423

---
+++ 2D_Midterm_0608422/Assets/程式/Player.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 300)]
+    public float speed = 15.5f;
+    [Header("名字"),]
+    public string  cName = "龍";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("變形元件")]
+    public Transform tra;
+    [Header("動畫元件")]
+    public Animator ani;
+    [Header("偵測範圍")]
+    public float rangeAttack = 2.5f;
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("撿取音效")]
+    public AudioClip soundAttack;
+
+
+    private void OnDrawGizmos()
+    {
+        //指定圖示顏色(紅,綠,藍,透明)
+        Gizmos.color = new Color(1, 0, 0, 0.4f);
+        //繪製圖示 球體(中心點,半徑)
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        float  h = joystick.Horizontal;
+
+        tra.Translate(h * speed * Time.deltaTime, 0, 0);
+        ani.SetFloat("水平", h);
+    }
+
+    public void Attack()
+    {
+
+        //音效來源,撥放一次(音效片段,音量)
+        aud.PlayOneShot(soundAttack, 0.5f);
+
+        //2D物理 圓形碰撞(中心點,半徑,方向,距離,圖層編號)
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, -transform.up, 0, 1 << 8);
+
+        //如果 碰到物件存在 並且 碰到的物件 標籤 為道具 就取得道具腳本並呼叫掉落道具方法
+        if (hit && hit.collider.tag == "道具") hit.collider.GetComponent<Item>().DropProp();
+
+    }
+    private void Update()
+    {
+        Move();
+
+    }
+    /*[Header("吃金塊音效")]
+    public AudioClip soundEat;
+    [Header("金幣數量")]
+    public Text textCoin;
+
+
+    private int coin;
+
+
+    //觸發事件-進入:兩個物件必須有一個勾選 Is Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "金塊")
+        {
+            coin++;
+            aud.PlayOneShot(soundEat);
+            Destroy(collision.gameObject);
+            textCoin.text = "金幣:" + coin;
+        }
+    }*/
+
+}
---
+++ 2D_Midterm_0608422/Assets/程式/TriggerArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    //陣列 在類型後面加上中括號
+    //用於保存相同類型的複數資料
+    [Header("要關閉的木頭")]
+    public GameObject[] stones;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "木頭")
+            stones[0].SetActive(false);
+        stones[1].SetActive(false);
+    }
+}
